31cec9ed8c246c4022d65ebbaa004e56fb02045b
mrhaav <62175065+mrhaav@users.noreply.github.com>
2024-04-16 22:23:58 +0200

    Create uqmi_led.sh

---
+++ uqmi_test/uqmi_led.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+rssi=$1
+
+LED1=$(readlink -f /sys/class/leds/white:signal1)
+LED2=$(readlink -f /sys/class/leds/white:signal2)
+LED3=$(readlink -f /sys/class/leds/white:signal3)
+
+if [ "${rssi}" -eq -200 ]
+then
+	echo 0 > $LED1/brightness
+	echo 0 > $LED2/brightness
+	echo 0 > $LED3/brightness
+elif [ "${rssi}" -le -90 ]
+then
+	echo 255 > $LED1/brightness
+	echo 0 > $LED2/brightness
+	echo 0 > $LED3/brightness
+elif [ "${rssi}" -le -70 ]
+then
+	echo 255 > $LED1/brightness
+	echo 255 > $LED2/brightness
+	echo 0 > $LED3/brightness
+else
+	echo 255 > $LED1/brightness
+	echo 255 > $LED2/brightness
+	echo 255 > $LED3/brightness
+fi
